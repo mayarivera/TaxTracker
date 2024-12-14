6002d04a2e2b1463941494c706087a3472ef54d8
Maya Rivera <97971264+mayarivera@users.noreply.github.com>
2024-12-14 17:09:09 -0500

    Update TaxTracker.py

--- TaxTracker.py
+++ TaxTracker.py
@@ -150,3 +150,6 @@ if __name__ == '__main__':
 
     # Run the app
     app.run(debug=True, host='0.0.0.0', port=8080)
+#Used prior project for flask outline and made alterations for project specific criteria
+#Used Chatgpt, Geeks for Geeks, Github, StackOverflow, Jetbrains, Medium
+#Had some trouble getting the tax rate and connecting to the port
